95eb6b6410cc21778ad5345636fd70be17947972
adamlahbib <adem.lahbib@insat.ucar.tn>
2023-04-19 14:37:01 +0100

    lab2

--- lab2/spark_batch/src/main/java/tn/insat/tp21/WordCountTask.java
+++ lab2/spark_batch/src/main/java/tn/insat/tp21/WordCountTask.java
@@ -1,4 +1,5 @@
-package tn.insat.tp1;
+package tn.insat.tp21;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -10,20 +11,20 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 import static jersey.repackaged.com.google.common.base.Preconditions.checkArgument;
-public class WordCount {
-    private static final Logger LOGGER = LoggerFactory.getLogger(WordCount.class);
-  
+public class WordCountTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WordCountTask.class);
+
     public static void main(String[] args) {
         checkArgument(args.length > 1, "Please provide the path of input file and output dir as parameters.");
-        new WordCount().run(args[0], args[1]);
+        new WordCountTask().run(args[0], args[1]);
     }
-  
+
     public void run(String inputFilePath, String outputDir) {
-  
-        SparkConf conf = new SparkConf().setAppName(WordCount.class.getName());
-  
+        SparkConf conf = new SparkConf()
+                .setAppName(WordCountTask.class.getName());
+
         JavaSparkContext sc = new JavaSparkContext(conf);
-  
+
         JavaRDD<String> textFile = sc.textFile(inputFilePath);
         JavaPairRDD<String, Integer> counts = textFile
                 .flatMap(s -> Arrays.asList(s.split("\t")).iterator())
@@ -31,4 +32,4 @@ public class WordCount {
                 .reduceByKey((a, b) -> a + b);
         counts.saveAsTextFile(outputDir);
     }
-  }
+}
